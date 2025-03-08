18664ec8ef9fd4e2cd9e02d26ab2d058d0047c53
Martin Prochazka <240970@vutbr.cz>
2025-03-08 14:54:07 +0100

    add: dummy module

---
+++ example-service-1/src/test/java/cz/fi/muni/pa165/exampleservice1/ExampleService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.fi.muni.pa165.exampleservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleService1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
