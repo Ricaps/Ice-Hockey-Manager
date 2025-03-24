b4b8a48944659b2160491b33d9a2064138ca1b7a
Matej Keznikl <xkeznikl@fi.muni.cz>
2025-03-24 08:54:03 +0100

    MicroServices structure

---
+++ game-service/src/test/java/cz/fi/muni/pa165/gameservice/GameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.fi.muni.pa165.gameservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
