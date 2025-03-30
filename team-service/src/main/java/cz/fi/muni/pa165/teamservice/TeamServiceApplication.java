0a08a9d865c2bafe5ac2c67601e309ebbe7282cc
Martin Procházka <xprocha4@fi.muni.cz>
2025-03-30 19:21:28 +0200

    Spring Javaformat & Open API YAML generation

--- team-service/src/main/java/cz/fi/muni/pa165/teamservice/TeamServiceApplication.java
+++ team-service/src/main/java/cz/fi/muni/pa165/teamservice/TeamServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TeamServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TeamServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TeamServiceApplication.class, args);
+	}
 
 }
--- world-list-service/src/main/java/cz/fi/muni/pa165/userservice/WorldListServiceApplication.java
+++ world-list-service/src/main/java/cz/fi/muni/pa165/userservice/WorldListServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorldListServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WorldListServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(WorldListServiceApplication.class, args);
+	}
 
 }
