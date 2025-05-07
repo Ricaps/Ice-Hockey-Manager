78bf961c1db40537a41207f18ab76aa0e0026e2c
Martin Procházka <xprocha4@fi.muni.cz>
2025-05-07 15:54:33 +0200

    Game Service - Actuator security

--- game-service/src/main/java/cz/fi/muni/pa165/gameservice/security/SecurityConfig.java
+++ game-service/src/main/java/cz/fi/muni/pa165/gameservice/security/SecurityConfig.java
@@ -1,12 +1,17 @@
-package cz.fi.muni.pa165.gameservice.config;
+package cz.fi.muni.pa165.gameservice.security;
 
+import cz.fi.muni.pa165.gameservice.security.filters.ActuatorTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @Profile({ "!test", "security-test" })
@@ -16,8 +21,27 @@ public class SecurityConfig {
 
 	public static final String SCOPE_TEST_READ = "SCOPE_test_read";
 
+	private final ActuatorTokenFilter actuatorTokenfilter;
+
+	@Autowired
+	public SecurityConfig(ActuatorTokenFilter actuatorTokenfilter) {
+		this.actuatorTokenfilter = actuatorTokenfilter;
+	}
+
 	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	@Order(1)
+	SecurityFilterChain prometheusFilterChain(HttpSecurity http) throws Exception {
+		http.securityMatcher("/actuator/**")
+			.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+			.addFilterBefore(actuatorTokenfilter, BasicAuthenticationFilter.class)
+			.csrf(AbstractHttpConfigurer::disable);
+
+		return http.build();
+	}
+
+	@Bean
+	@Order(2)
+	SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(x -> x
 			.requestMatchers("/", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**", "/v3/api-docs.yaml")
 			.permitAll()
