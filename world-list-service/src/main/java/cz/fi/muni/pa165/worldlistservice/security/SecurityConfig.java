700a3a619f625e724bbf68cd54b5bf368772c913
Martin Procházka <xprocha4@fi.muni.cz>
2025-05-09 19:13:19 +0200

    World list - fix actuator endpoints (added security token)

--- world-list-service/src/main/java/cz/fi/muni/pa165/worldlistservice/security/SecurityConfig.java
+++ world-list-service/src/main/java/cz/fi/muni/pa165/worldlistservice/security/SecurityConfig.java
@@ -1,12 +1,16 @@
-package cz.fi.muni.pa165.worldlistservice.config;
+package cz.fi.muni.pa165.worldlistservice.security;
 
+import cz.fi.muni.pa165.worldlistservice.security.filters.ActuatorTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @Profile({ "!test", "security-test" })
@@ -16,7 +20,25 @@ public class SecurityConfig {
 
 	public static final String SCOPE_TEST_READ = "SCOPE_test_read";
 
+	private final ActuatorTokenFilter actuatorTokenFilter;
+
+	public SecurityConfig(ActuatorTokenFilter actuatorTokenFilter) {
+		this.actuatorTokenFilter = actuatorTokenFilter;
+	}
+
 	@Bean
+	@Order(1)
+	SecurityFilterChain prometheusFilterChain(HttpSecurity http) throws Exception {
+		http.securityMatcher("/actuator/**")
+			.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+			.addFilterBefore(actuatorTokenFilter, BasicAuthenticationFilter.class)
+			.csrf(AbstractHttpConfigurer::disable);
+
+		return http.build();
+	}
+
+	@Bean
+	@Order(2)
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(x -> x
 			.requestMatchers("/", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**", "/v3/api-docs.yaml")
