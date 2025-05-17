daa326f14c3679b4c8f209275bb6f73a69e5fec8
Martin Procházka <xprocha4@fi.muni.cz>
2025-05-17 20:16:52 +0200

    Add error responses to OpenAPI globally

--- team-service/src/main/java/cz/fi/muni/pa165/teamservice/config/OpenAPIConfig.java
+++ team-service/src/main/java/cz/fi/muni/pa165/teamservice/config/OpenAPIConfig.java
@@ -2,6 +2,7 @@ package cz.fi.muni.pa165.teamservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OpenApiCustomizer;
@@ -43,7 +44,18 @@ public class OpenAPIConfig {
 					.description(
 							"Please provide valid access token. You can get it via OAuth2 client at http://localhost:8084"));
 			openApi.addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_BEARER));
+			addGlobalResponse(openApi, "401",
+					new ApiResponse().description("You are not authorized to access this resource!"));
+			addGlobalResponse(openApi, "403",
+					new ApiResponse().description("You don't have required permissions to access this resource!"));
 		};
+	}
+
+	private void addGlobalResponse(OpenAPI openApi, String operationName, ApiResponse apiResponse) {
+		openApi.getPaths()
+			.values()
+			.forEach(path -> path.readOperations()
+				.forEach(operation -> operation.getResponses().addApiResponse(operationName, apiResponse)));
 	}
 
 }
